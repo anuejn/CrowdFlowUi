4b9d3017af4fd64f459df61a1624fe9e94d71b79
jaro <->
2015-10-17 16:24:29 +0200

    wrote new heat map draw function

--- stuff.js
+++ stuff.js
@@ -1,69 +1,68 @@
-  
-        function WebSocketStart()
-         {
-            //alert("running");
-             
-            var heatmapInstance = h337.create({
-                container: document.querySelector('#map')
-            });
 
-            if ("WebSocket" in window)
-            {
-               var ws = new WebSocket("ws://echo.websocket.org");
-                var packages = 0;
-               ws.onopen = function()
-               {
-                   $("header").css("background", "#0FFF0F")
-                   $("header").text("Connected!");
-                   
-                   // now generate some random data
-var points = [];
-var max = 0;
-var width = 840;
-var height = 400;
-var len = 200;
+function WebSocketStart()
+ {
+    var heatmapInstance = h337.create({
+        container: document.querySelector('#map')
+    });
 
-while (len--) {
-  var val = Math.floor(Math.random()*100);
-  max = Math.max(max, val);
-  var point = {
-    x: Math.floor(Math.random()*width),
-    y: Math.floor(Math.random()*height),
-    value: val
-  };
-  points.push(point);
+    if ("WebSocket" in window)
+    {
+       var ws = new WebSocket("ws://echo.websocket.org");
+        var packages = 0;
+       ws.onopen = function()
+       {
+           $("header").css("background", "#0FFF0F")
+           $("header").text("Connected!");
+       };
+
+       ws.onmessage = function (evt) 
+       {
+            var received_msg = evt.data;
+           packages += 1;
+            $("footer").text("(" + packages + " Packages recived)");
+
+       };
+
+       ws.onclose = function()
+       {
+          // websocket is closed.
+          //$("messages").html += "Websocket closed!"
+           $("header").css("background", "#FF0F0F")
+           $("header").text("not running (try to reload)");
+       };
+    }
+
+    else
+    {
+       // The browser doesn't support WebSocket
+       alert("WebSocket NOT supported by your Browser!");
+        window.close();
+    }
+ }
+
+function paintHeatMap(var points, var heatMap, var domElement) {
+    
+            var newPoints = [];
+            var max = 0;
+            var width = domElement.width();
+            var height = domElement.height();
+            
+            for(var i = 0; i < points.length; i++) {
+              var val = 1;
+              max = Math.max(max, val);
+              var point = {
+                x: points[i].x / 1.0 + 0, //cahnge me to an sensfull value!
+                y: points[i].y / 1.0 + 0,//cahnge me to an sensfull value!
+                value: val
+              };
+              points.push(point);
+            }
+            // heatmap data format
+            var data = {
+              max: max, 
+              data: points 
+            };
+            // if you have a set of datapoints always use setData instead of addData
+            // for data initialization
+            heatmapInstance.setData(data);
 }
-// heatmap data format
-var data = { 
-  max: max, 
-  data: points 
-};
-// if you have a set of datapoints always use setData instead of addData
-// for data initialization
-heatmapInstance.setData(data);
-               };
-				
-               ws.onmessage = function (evt) 
-               {
-                    var received_msg = evt.data;
-                   packages += 1;
-                    $("footer").text("(" + packages + " Packages recived)");
-                   
-               };
-				
-               ws.onclose = function()
-               {
-                  // websocket is closed.
-                  //$("messages").html += "Websocket closed!"
-                   $("header").css("background", "#FF0F0F")
-                   $("header").text("not running (try to reload)");
-               };
-            }
-             
-            else
-            {
-               // The browser doesn't support WebSocket
-               alert("WebSocket NOT supported by your Browser!");
-                window.close();
-            }
-         }
