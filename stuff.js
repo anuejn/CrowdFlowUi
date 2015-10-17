0012ca5ef5281b62f91efb0c778ccfb7af9689d0
jaro <->
2015-10-17 15:33:39 +0200

    now we can do some random heatmap stuff

--- stuff.js
+++ stuff.js
@@ -1,8 +1,12 @@
-var usersAndPositions;
-           
+  
         function WebSocketStart()
          {
             //alert("running");
+             
+            var heatmapInstance = h337.create({
+                container: document.querySelector('#map')
+            });
+
             if ("WebSocket" in window)
             {
                var ws = new WebSocket("ws://echo.websocket.org");
@@ -11,10 +15,36 @@ var usersAndPositions;
                {
                    $("header").css("background", "#0FFF0F")
                    $("header").text("Connected!");
+                   
+                   // now generate some random data
+var points = [];
+var max = 0;
+var width = 840;
+var height = 400;
+var len = 200;
+
+while (len--) {
+  var val = Math.floor(Math.random()*100);
+  max = Math.max(max, val);
+  var point = {
+    x: Math.floor(Math.random()*width),
+    y: Math.floor(Math.random()*height),
+    value: val
+  };
+  points.push(point);
+}
+// heatmap data format
+var data = { 
+  max: max, 
+  data: points 
+};
+// if you have a set of datapoints always use setData instead of addData
+// for data initialization
+heatmapInstance.setData(data);
                };
 				
                ws.onmessage = function (evt) 
-               { 
+               {
                     var received_msg = evt.data;
                    packages += 1;
                     $("footer").text("(" + packages + " Packages recived)");
